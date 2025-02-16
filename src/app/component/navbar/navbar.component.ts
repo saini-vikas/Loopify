9bf262ca54c72e1a0ebbcecb67b7aabe93797630
Vikas Saini <vikassaini883@gmail.com>
2025-02-16 15:28:14 -0500

    added profile menu

--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -4,12 +4,30 @@ import { MenubarModule } from 'primeng/menubar';
 import { Avatar } from 'primeng/avatar';
 import { InputTextModule } from 'primeng/inputtext';
 import { FormsModule } from '@angular/forms';
+import { MenuModule } from 'primeng/menu';
+import { Button } from 'primeng/button';
 @Component({
   selector: 'app-navbar',
-  imports: [MenubarModule, RouterLink, Avatar, InputTextModule, FormsModule],
+  imports: [
+    MenubarModule,
+    RouterLink,
+    Avatar,
+    InputTextModule,
+    FormsModule,
+    MenuModule,
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
   searchInput = signal('');
+  activeUser = signal('Vikas');
+  profileMenu = signal([
+    { title: 'View profile', link: '/profile' },
+    { title: 'My playlists', link: '/myplaylists' },
+  ]);
+
+  openAvatar() {
+    console.log('Avatar clicked');
+  }
 }
